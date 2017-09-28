c3e42554c183a8ecb259a6ba7b7c2fb9976667d1
Hieu Hoang <hieuhoang@gmail.com>
2017-09-28 19:10:07 +0100

    switch FLOAT to float

--- src/amun/gpu/mblas/matrix_functions.cu
+++ src/amun/gpu/mblas/matrix_functions.cu
@@ -386,8 +386,10 @@ Matrix& Prod(cublasHandle_t handle, Matrix& C, const Matrix& A, const Matrix& B,
   //cerr << "FLOAT Prod" << endl;
   assert((A.dim(2) == A.dim(3) == 1) || (B.dim(2) == B.dim(3) == 1));
 
-  Matrix::value_type alpha = float2half_rn(1.0);
-  Matrix::value_type beta = float2half_rn(0.0);
+  Matrix::value_type alpha = 1.0;
+  Matrix::value_type beta = 0.0;
+  //Matrix::value_type alpha = float2half_rn(1.0);
+  //Matrix::value_type beta = float2half_rn(0.0);
 
   size_t m = A.dim(0) * A.dim(2) * A.dim(3);
   size_t k = A.dim(1);
@@ -444,7 +446,7 @@ Matrix& Prod(cublasHandle_t handle, Matrix& C, const Matrix& A, const Matrix& B,
                            const float           *beta,
                            float           *C, int ldc)
    */
-  cublasHgemm(handle, opB, opA,
+  cublasSgemm(handle, opB, opA,
               n, m, k,
               &alpha,
               B.data(), ldb,
